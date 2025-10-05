b7da03c1d9f2d0aed95224ad745e0395f3412f3d
tiko-ops <tishko.shafiq@gmail.com>
2025-10-05 22:00:59 +0200

    Create page.tsx

---
+++ app/villkor/page.tsx
@@ -0,0 +1,14 @@
+export default function Villkor() {
+  return (
+    <article style={{ lineHeight: 1.6 }}>
+      <h1>Användarvillkor</h1>
+      <ul>
+        <li>Max 60 rättningar per timme och IP-adress. Varning visas vid 80% av gränsen.</li>
+        <li>Tjänsten rättar enbart svenska texter.</li>
+        <li>Tjänsten är kostnadsfri och tillhandahålls i befintligt skick.</li>
+        <li>Vi kan tillfälligt blockera användning vid misstänkt missbruk.</li>
+      </ul>
+      <p>Kontakt: <a href="mailto:kundservice@mamosta.se">kundservice@mamosta.se</a></p>
+    </article>
+  );
+}
